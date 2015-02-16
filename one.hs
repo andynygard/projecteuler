edf1a85b0e1686bb59908e03f04559fef10dca30
Andy Nygard <andreas.nygard@gmail.com>
2015-02-17 00:08:58 +1100

    First one

--- one.hs
+++ one.hs
@@ -1,1 +1,5 @@
-main = putStrLn "Hello, World!"
+import Data.List (union)
+
+main = print . sum $ union (multiples 3) (multiples 5)
+    where
+        multiples a = takeWhile (< 1000) $ map (* a) [1..]
