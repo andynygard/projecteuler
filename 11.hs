9ec716020b2c1d072e61b8940d4026464ebd6b67
Andy Nygard <andreas.nygard@gmail.com>
2015-03-18 23:45:05 +1100

    Working on 11

--- 11.hs
+++ 11.hs
@@ -2,12 +2,21 @@ import Data.Array
 
 dataFile = "11_data.txt"
 
-calcGrid :: [[Int]] -> Int
-calcGrid g = g!!0!!1 -- Select some random cell for now to test with
+calcGrid n g = maximum . concat $ [[hs,vs,ds] | let is = range . bounds $ g,
+                                                let inBounds x = x `elem` is,
+                                                let prod f ix = product . map (g!) . filter inBounds . take n . f $ ix,
+                                                i <- is,
+                                                let hs = prod horz i,
+                                                let vs = prod vert i,
+                                                let ds = prod diag i]
+    where
+        horz i@(x,y) = i : (map (\n -> (x+n,y)) $ [1..])
+        vert i@(x,y) = i : (map (\n -> (x,y+n)) $ [1..]) 
+        diag i@(x,y) = i : (map (\n -> (x+n,y+n)) $ [1..])
 
 io f = do
     s <- readFile dataFile
-    let rows = map (map read . words) . lines $ s
-    putStr . show . f $ rows
+    let grid = listArray ((1,1),(20,20)) . map (read :: String -> Int) . words $ s
+    putStr . show . f $ grid
 
-main = io $ calcGrid
+main = io . calcGrid $ 4
